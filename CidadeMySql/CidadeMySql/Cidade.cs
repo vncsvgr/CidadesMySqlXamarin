79248628de76f8a6a8e9ed9743ec69092cc6eb72
Usuario <Usuario@CPD03-M12>
2022-05-04 21:46:30 -0300

    Adicionar arquivos de projeto.

---
+++ CidadeMySql/CidadeMySql/Cidade.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySqlConnector;
+
+namespace CidadeMySql
+{
+    public class Cidade
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string uf { get; set; }
+
+        MySqlConnection conexao;
+        MySqlCommand comando;
+        MySqlDataReader rConsulta;
+        string strConexao = "server=br536.hostgator.com.br;uid=dsetec86_aula;pwd=etecjau;database=dsetec86_bd_xamarin";
+
+        public async void Incluir()
+        {
+            try
+            {
+                conexao = new MySqlConnection(strConexao);
+                conexao.Open();
+                comando = new MySqlCommand("insert into cidades (nome, uf) " +
+                                                       "values (@nome, @uf)", conexao);
+                comando.Parameters.AddWithValue("@nome", nome);
+                comando.Parameters.AddWithValue("@uf", uf);
+                comando.ExecuteNonQuery();
+                conexao.Close();
+                await App.Current.MainPage.DisplayAlert("Inclusão", "Cidade incluida com sucesso!", "OK");
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        public async void Consultar()
+        {
+            try
+            {
+                conexao = new MySqlConnection(strConexao);
+                conexao.Open();
+                comando = new MySqlCommand("select * from cidades where id = @id", conexao);
+                comando.Parameters.AddWithValue("@id", id);
+                rConsulta = comando.ExecuteReader();
+                if (rConsulta.Read())
+                {
+                    nome = rConsulta["NOME"].ToString();
+                    uf = rConsulta["UF"].ToString();
+                }
+                else
+                {
+                    await App.Current.MainPage.DisplayAlert("Consulta", "Cidade não encontrada!", "OK");
+                }
+                conexao.Close();
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        public async void Atualizar()
+        {
+            try
+            {
+                conexao = new MySqlConnection(strConexao);
+                conexao.Open();
+                comando = new MySqlCommand("update cidades set nome = @nome, @uf = uf " +
+                    "where id = @id", conexao);
+                comando.Parameters.AddWithValue("@nome", nome);
+                comando.Parameters.AddWithValue("@uf", uf);
+                comando.Parameters.AddWithValue("@id", id);
+                comando.ExecuteNonQuery();
+                conexao.Close();
+                await App.Current.MainPage.DisplayAlert("Alteração", "Cidade atualizada com sucesso!", "OK");
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Erro", ex.Message, "OK");
+
+            }
+        }
+    }
+}
---
+++ CidadeMySql/CidadeMySql/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CidadeMySql
+{
+    public partial class MainPage : ContentPage
+    {
+        Cidade c;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtID.Text))
+            {
+                DisplayAlert("Atenção", "Informe o ID para pesquisa", "OK");
+                return;
+            }
+            c = new Cidade
+            {
+                id = int.Parse(txtID.Text)
+            };
+            c.Consultar();
+            txtNome.Text = c.nome;
+            txtUF.Text = c.uf;
+        }
+
+        private void btnLimpar_Clicked(object sender, EventArgs e)
+        {
+            txtID.Text = string.Empty;
+            txtNome.Text = string.Empty;
+            txtUF.Text = string.Empty;
+        }
+
+        private void btnAdicionar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAtualizar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNome.Text) || string.IsNullOrEmpty(txtUF.Text))
+            {
+                DisplayAlert("Atenção", "Favor completar as informações do cadastro", "OK");
+                return;
+            }
+
+            c = new Cidade
+            {
+                nome = txtNome.Text,
+                uf = txtUF.Text
+            };
+            c.Incluir();
+            btnLimpar_Clicked(sender, e);
+        }
+
+        private void btnExcluir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFinalizar_Clicked(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
